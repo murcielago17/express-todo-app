492b01802fbc239daedce6be79137b3cc24e50c1
murcielago17 <murcielago17@hotmail.com>
2015-11-11 12:18:30 -0800

    // create a server route to handle de POST request

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = requires('express');
+var app = express();
+	
+
+	// create a server route to handle de POST request
+	app.post('/api/todos', function(req,res){
+		var newTodo =req.body;
+
+
+
+
+	});
