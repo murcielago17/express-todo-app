0f13868ba6dc52d93712168d60b39e7529773f13
murcielago17 <murcielago17@hotmail.com>
2015-11-11 15:09:35 -0800

    //  2) Steps to Read // 2-1) Create a server route to handle the GET request.

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = requires('express');
 var app = express();
 	
 
-	// 1) create a server route to handle de POST request
+	// 1) Steps to Create
+		// 1-1) Create a server route to handle de POST request
 	app.post('/api/todos', function(req,res){
-		// 1-a) Create a new todo with form data
+		// 1-2) Create a new todo with form data
 		var newTodo =req.body;
 
 
-		// 1-b) set a sequential ID to the Todo
+		// 1-3) set a sequential ID to the Todo
 		if (todos.length > 0) {
 
 			NewTodo._id = todos[todos.length - 1]._id + 1;
@@ -17,18 +18,22 @@ var app = express();
 		}
 
 
-		// 1-c) add a new todo to the todos array
+		// 1-4) add a new todo to the todos array
 		todos.push(newTodo);
 
 
-		// 1-d) Send new todo as JSON response
+		// 1-5) Send new todo as JSON response
 		res.json(newTodo);
 
 	});
 
-	//  2) Create a server route to handle the GET request.
+	//  2) Steps to Read
+		// 2-1) Create a server route to handle the GET request.
 	app.get('/api/todos/:id', function(req,res) {
-		
+		var todoId = parseInt(req.params.id);
+		var foundTodo = todos.filter(function(todo) {
+			return todo._id == todoId;
+		})[0];
 
 	});
 
