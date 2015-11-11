ecb6004e0acdcd4d06513b12b3b5ac89fc808174
murcielago17 <murcielago17@hotmail.com>
2015-11-11 12:19:52 -0800

    // Create a new todo with form data

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 
 	// create a server route to handle de POST request
 	app.post('/api/todos', function(req,res){
+		// Create a new todo with form data
 		var newTodo =req.body;
 
 
