f1081d7da36c6bdf6b3a495e6cd17fe4a2372874
murcielago17 <murcielago17@hotmail.com>
2015-11-12 23:54:53 -0800

    mangoose methods - on todo.js // get all todos

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,6 @@
 // create the model schema, and export 
 // it so that you can require it in 
 // other parts of your app.
-
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
@@ -13,3 +12,13 @@ var TodoSchema = new Schema ({
 
 var Todo = mongoose.model('Todo', TodoSchema);
 module.exports = Todo;
+
+// get all todos
+  app.get('/api/todos', function (req, res) {
+    // find all todos in db
+    Todo.find(function (err, allTodos) {
+      res.json({ todos: allTodos });
+    });
+  });
+
+  
